4f8e4c8b3df91b21c2fc082da77cc3a55706576d
chloeblanpain <chloe.blanpain@icloud.com>
2025-02-04 15:04:19 +0100

    mini rapport réalisé avec JM

--- testggplot2.R
+++ testggplot2.R
@@ -4,19 +4,36 @@
 library(ggplot2)
 
 ##
+# Visualisation de la structure du jeu de données
 
 data(diamonds)
 head(diamonds)
+names(diamonds) 
 
+summary(diamonds) # Résumé statistique des variables
  
+ggplot(diamonds, aes(x = price)) +
+  geom_histogram(binwidth = 500, fill = "steelblue", color = "lightpink", alpha = 0.5) +
+  labs(title = "Distribution du prix des diamants", x = "prix", y = "nombre de diamants") +
+  theme_void()
+
+
 #évolution du prix en fonction du carat
 ggplot(data=diamonds, mapping= aes(x=price, y= carat, color = color))+  
-  geom_point()+
+  geom_point(alpha=0.5, size=5)+
+  scale_color_brewer(palette = "Set1")+
   geom_smooth( method=lm , color = "yellow") +
   theme_dark()+
-  labs(title = "Prix des diamants en fonction du Carat",
-       x="Prix (en Euro)", y="Carat",
-       subtitle = "les diamants, c'est cher")
+  labs(title = "Prix des diamants en fonction du poids (carat)",
+       x="Prix (en Euro)", y="Poid (en carat)")
+
+
+#Histogramme du prix en fonction de la couleur
+ggplot(diamonds, aes(x = price, fill = color)) +
+  geom_histogram(binwidth = 250, alpha = 0.7, position = "identity", color = "pink") +
+  facet_wrap(~ color) +
+  labs(title = "Prix des diamants en fonction de la couleur", x = "prix", y = "nombre de diamants") +
+  theme_minimal()
 
 
 
